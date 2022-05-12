3837774843cf2f2ae150a5595ef5eee7bcc5ed66
DedicatedDev <topdev323@gmail.com>
2022-05-12 20:53:27 +0300

    admin test in mocha

--- packages/postchain/test/bridge.test.ts
+++ packages/postchain/test/bridge.test.ts
@@ -1,47 +1,40 @@
 import { expect } from "chai";
 import { Utils } from "./utils";
-
 import {
   op,
-  Postchain,
-  SingleSignatureAuthDescriptor,
-  FlagsType,
   User,
   KeyPair,
-  Operation,
-  Blockchain,
-  Asset,
+  Blockchain
 } from "ft3-lib";
+import path from "path";
+import * as dotenv from "dotenv";
+dotenv.config({ path: path.join(__dirname, "../../../", ".env") });
 
 describe("✅ bridge", async () => {
   let client: Blockchain;
   let chainId: string;
+  let oracleAdmin: User;
   beforeEach(async () => {
     const chain = await Utils.getClient();
     client = chain.client;
     chainId = chain.chainId;
+    
+    //register admin
+    oracleAdmin = Utils.createUser(process.env.ORACLE_ADMIN);
+    //await client.call(op("bridge.add_contract", "eth_bsc", oracleAdmin.keyPair.pubKey, "ERC1155"), oracleAdmin);
   });
 
   it("add contracts", async () => {
-    const keyPair = new KeyPair().makeKeyPair();
-    const user = new User(
-      keyPair,
-      new SingleSignatureAuthDescriptor(keyPair.pubKey, [FlagsType.Account, FlagsType.Transfer])
-    );
-    await client.call(op("bridge.add_contract", "eth_bsc", keyPair.pubKey, "ERC1155"), user);
+    await client.call(op("bridge.add_contract", "eth_bsc", oracleAdmin.keyPair.pubKey, "ERC1155"), oracleAdmin);
   });
 
   it("transfer ownership", async () => {
     const keyPair = new KeyPair().makeKeyPair();
-    const user = new User(
-      keyPair,
-      new SingleSignatureAuthDescriptor(keyPair.pubKey, [FlagsType.Account, FlagsType.Transfer])
-    );
     const encoder = new TextEncoder();
     const bytes = encoder.encode(keyPair.pubKey).join("");
     console.log("====================================");
     console.log(bytes);
     console.log("====================================");
-    await client.call(op("bridge.transfer_ownership", "eth_main", bytes, 1, bytes), user);
+    await client.call(op("bridge.transfer_ownership", "eth_main", bytes, 1, bytes), oracleAdmin);
   });
 });
